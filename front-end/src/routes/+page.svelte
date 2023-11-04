beb5c9556bb49226d91632497b22d6df20ac1b29
Dan Fiumara <technodan143@gmail.com>
2023-11-04 18:36:33 -0400

    Second
    added static data

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -23,8 +23,9 @@
   }
   .user-summary {
     height: 400px;
-    width: 300px;
+    width: 400px;
     margin: 20px;
+    padding: 20px;
     border-radius: 30px;
     background-color: orange;
   }
@@ -33,14 +34,39 @@
     text-align: center;
     font-size: 24px;
   }
+  .summary-info {
+    display: flex;
+    text-align: left;
+    align-items: center;
+    margin-left: 10px;
+    font-size: 24px;
+    list-style-type: none;
+  }
+  .summary-info img {
+    width: 50px;
+    margin-right: 10px;
+  }
 </style>
 
 <script>
   import { onMount } from "svelte";
   import { apiData, familyMembers } from "./store.js";
   //$: dosesOnHand = 10;
+  function notifyToday(doseToday){
+    return doseToday ? "Don't forget to take your medicine today!" : "You have already taken your medicine today!";
+  }
 
-
+  onMount(async () => {
+  fetch("")
+  .then(response => response.json())
+  .then(data => {
+		console.log(data);
+    apiData.set(data);
+  }).catch(error => {
+    console.log(error);
+    return [];
+  });
+});
 </script>
 
 <div class="parent-container">
@@ -49,9 +75,11 @@
   <!-- <p>You have {dosesOnHand} doses remaining</p> -->
 
   <div class="summary-container">
-    {#each $familyMembers as name}
+    {#each $familyMembers as person}
       <div class="user-summary">
-        <h3 class="summary-title">{name}</h3>
+        <h3 class="summary-title">{person.name}</h3>
+        <li class="summary-info">{notifyToday(person.doseToday)}</li>
+        <li class="summary-info"><img src="./pills-solid.svg">{person.dosagesLeft} Dosages Left</li>
       </div>
     {/each}
   </div>
--- front-end/src/routes/store.js
+++ front-end/src/routes/store.js
@@ -8,5 +8,17 @@ export const familyMembers = derived(apiData, ($apiData) => {
   }
   return [];
 });
+
+export const = remainingPills derived(apiData, ($apiData) => {
+  if ($apiData.) {
+    return $apiData.users.map(user => user.numPills);
+  }
+  return [];
+});
 */
-export const familyMembers = writable(["Alice", "Bob", "Om", "Dan"]);
+export const familyMembers = writable([
+  { "name": "Abdul", "dosagesLeft": "6", "doseToday": 1 },
+  { "name": "Amaan", "dosagesLeft": "23", "doseToday": 0 },
+  { "name": "Dan", "dosagesLeft": "18", "doseToday": 0 },
+  { "name": "Om", "dosagesLeft": "5", "doseToday": 1 },
+]);
